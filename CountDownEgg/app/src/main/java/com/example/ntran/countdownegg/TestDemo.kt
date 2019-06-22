4485f59e38fc512df52bd3e216b3ad37d23d7c4f
Trần Phú Nguyện 1 <tranphunguyen.fit@gmail.com>
2019-06-22 21:12:17 +0700

    Test Junit

---
+++ CountDownEgg/app/src/main/java/com/example/ntran/countdownegg/TestDemo.kt
@@ -0,0 +1,7 @@
+package com.example.ntran.countdownegg
+
+class TestDemo {
+    fun add(a: Int, b: Int) : Int {
+        return a + b
+    }
+}
--- CountDownEgg/app/src/test/java/com/example/ntran/countdownegg/ExampleUnitTest.kt
+++ CountDownEgg/app/src/test/java/com/example/ntran/countdownegg/ExampleUnitTest.kt
@@ -4,14 +4,14 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class TestDemoUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun add_isCorrect() {
+        assertEquals(4,TestDemo().add(2,2))
+    }
+
+    @Test
+    fun add_isUnCorrect() {
+        assertEquals(4,TestDemo().add(3,2))
     }
 }
