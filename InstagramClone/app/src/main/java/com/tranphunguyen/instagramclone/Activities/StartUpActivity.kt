eb848d4906e5afc0afc8156e0660a1a2c6be1704
Trần Phú Nguyện 1 <tranphunguyen.fit@gmail.com>
2019-07-04 21:45:32 +0700

    Tao app instagram clone

---
+++ InstagramClone/app/src/main/java/com/tranphunguyen/instagramclone/Activities/StartUpActivity.kt
@@ -0,0 +1,13 @@
+package com.tranphunguyen.instagramclone.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tranphunguyen.instagramclone.R
+
+class StartUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+    }
+}
